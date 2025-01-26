2d7be7e9dd42816b5cf3d74c895ce9955e74ac6f
Sardius <0xsardius@gmail.com>
2025-01-26 09:19:28 -0500

    actions, link and wallet defined

--- components/Demo.tsx
+++ components/Demo.tsx
@@ -1,11 +1,22 @@
-import { useEffect, useState } from "react";
-import sdk from "@farcaster/frame-sdk";
+import { useEffect, useCallback, useState } from "react";
+import sdk, { type FrameContext } from "@farcaster/frame-sdk";
+import { useAccount, useDisconnect, useConnect } from "wagmi";
+
+import { config } from "../components/providers/WagmiProvider";
+import { Button } from "./ui/Button";
 
 export default function Demo() {
   const [isSDKLoaded, setIsSDKLoaded] = useState(false);
+  const [context, setContext] = useState<FrameContext>();
+  const [isContextOpen, setIsContextOpen] = useState(false);
+
+  const { address, isConnected } = useAccount();
+  const { disconnect } = useDisconnect();
+  const { connect } = useConnect();
 
   useEffect(() => {
     const load = async () => {
+      setContext(await sdk.context);
       sdk.actions.ready();
     };
     if (sdk && !isSDKLoaded) {
@@ -14,11 +25,96 @@ export default function Demo() {
     }
   }, [isSDKLoaded]);
 
+  const toggleContext = useCallback(() => {
+    setIsContextOpen((prev) => !prev);
+  }, []);
+
+  const openUrl = useCallback(() => {
+    sdk.actions.openUrl("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+  }, []);
+
+  const close = useCallback(() => {
+    sdk.actions.close();
+  }, []);
+
+  if (!isSDKLoaded) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="w-[300px] mx-auto py-4 px-2">
       <h1 className="text-2xl font-bold text-center mb-4">
         Farcaster Frames v2 Demo
       </h1>
+
+      {/* Context */}
+      <div className="mb-4">
+        <h2 className="font-2xl font-bold">Context</h2>
+        <button
+          onClick={toggleContext}
+          className="flex items-center gap-2 transition-colors"
+        >
+          <span
+            className={`transform transition-transform ${
+              isContextOpen ? "rotate-90" : ""
+            }`}
+          >
+            ➤
+          </span>
+          Tap to expand
+        </button>
+
+        {isContextOpen && (
+          <div className="p-4 mt-2 bg-gray-100 dark:bg-gray-800 rounded-lg">
+            <pre className="font-mono text-xs whitespace-pre-wrap break-words max-w-[260px] overflow-x-">
+              {JSON.stringify(context, null, 2)}
+            </pre>
+          </div>
+        )}
+
+        <div>
+          <h2 className="font-2xl font-bold">Actions</h2>
+
+          <div className="mb-4">
+            <div className="p-2 bg-gray-100 dark:bg-gray-800 rounded-lg my-2">
+              <pre className="font-mono text-xs whitespace-pre-wrap break-words max-w-[260px] overflow-x-auto">
+                sdk.actions.openUrl
+              </pre>
+            </div>
+            <Button onClick={openUrl}>Open Link</Button>
+          </div>
+          <div className="mb-4">
+            <div className="p-2 bg-gray-100 dark:bg-gray-800 rounded-lg my-2">
+              <pre className="font-mono text-xs whitespace-pre-wrap break-words max-w-[260px] overflow-x-">
+                sdk.actions.close
+              </pre>
+            </div>
+            <Button onClick={close}>Close Frame</Button>
+          </div>
+
+          <div>
+            <h2 className="font-2xl font-bold">Wallet</h2>
+
+            {address && (
+              <div className="my-2 text-xs">
+                Address: <pre className="inline">{address}</pre>
+              </div>
+            )}
+
+            <div className="mb-4">
+              <Button
+                onClick={() =>
+                  isConnected
+                    ? disconnect()
+                    : connect({ connector: config.connectors[0] })
+                }
+              >
+                {isConnected ? "Disconnect" : "Connect"}
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
